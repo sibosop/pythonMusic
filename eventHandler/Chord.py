44119dff401b286f5f163b8e4fa170061687c886
sibosop <brian@eastshore.com>
2017-01-25 21:59:57 -0800

    added chords

---
+++ eventHandler/Chord.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import mido
+from EvUtil import getVal
+from Note import Note
+
+class Chord(object):
+  def __init__(self,chord,vel,len,chan): 
+    self.chord = chord
+    self.vel = vel
+    self.len = len
+    self.chan = chan
+    
+  def fire(self,ms):
+    playList = []
+    ch = self.chord
+    if ch.__class__.__name__ != 'list':
+      ch = self.chord()
+    for x in ch:
+      playList.append(getVal(x))
+    playVel = getVal(self.vel)
+    playChan = getVal(self.chan)
+    playLen = getVal(self.len)
+    for x in playList:
+      msg=mido.Message('note_on',note=x,velocity=playVel,channel=playChan)
+      ms.midiOut.send(msg)
+    if playVel != 0:
+      off=ms.count+playLen
+      for x in playList:
+        ms.addEvent(off,Note(note=x,vel=0,len=0,chan=playChan))
+      
